3c5a50606989fe493591b596bae1b8b4230009fa
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-09-29 23:57:29 -0400

    docs: adds some documentation to TransactionBound

--- src/transaction.rs
+++ src/transaction.rs
@@ -3,12 +3,39 @@ use std::time::SystemTime;
 
 use crate::proto::google::spanner::v1 as proto;
 
+/// Specifies the bounds withing wich to make reads in Spanner.
+///
+/// See [the Spanner Documentation](https://cloud.google.com/spanner/docs/reference/rpc/google.spanner.v1#google.spanner.v1.TransactionOptions.ReadOnly)
 #[derive(Clone, Debug)]
 pub enum TimestampBound {
+    /// Read at a timestamp where all previously committed transactions are visible.
+    ///
+    /// Strong reads are guaranteed to see the effects of all transactions that have committed before the start of the read.
+    /// Furthermore, all rows yielded by a single read are consistent with each other -- if any part of the read observes a transaction, all parts of the read see the transaction.
     Strong,
+
+    /// Executes all reads at the given timestamp.
+    ///
+    /// Unlike other modes, reads at a specific timestamp are repeatable; the same read at the same timestamp always returns the same data.
+    /// If the timestamp is in the future, the read will block until the specified timestamp, modulo the read's deadline.
+    ///
+    /// Useful for large scale consistent reads such as mapreduces, or for coordinating many reads against a consistent snapshot of the data.
     ReadTimestamp(SystemTime),
+
+    /// Executes all reads at a timestamp >= the provided timestamp.
+    ///
+    /// This is useful for requesting fresher data than some previous read,
+    /// or data that is fresh enough to observe the effects of some previously committed transaction whose timestamp is known.
     MinReadTimestamp(SystemTime),
+
+    /// Executes all reads at a timestamp that is `ExactStaleness` old. The timestamp is chosen soon after the read is started.
+    ///
+    /// Guarantees that all writes that have committed more than the specified number of seconds ago are visible.
     ExactStaleness(Duration),
+
+    /// Read data at a timestamp `>= now() - MaxStaleness` seconds.
+    ///
+    /// Guarantees that all writes that have committed more than the specified number of seconds ago are visible.
     MaxStaleness(Duration),
 }
 
