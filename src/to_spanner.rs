3be021748ff877435056410600aa99fa376b27c6
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-10-03 17:32:38 -0400

    feat: adds ToSpanner for arrays, adds tests

--- src/to_spanner.rs
+++ src/to_spanner.rs
@@ -44,6 +44,38 @@ where
     }
 }
 
+impl<T> ToSpanner for Vec<T>
+where
+    T: ToSpanner,
+{
+    fn to_spanner(&self) -> Result<Value, Error> {
+        let values = self
+            .iter()
+            .map(|v| v.to_spanner())
+            .collect::<Result<Vec<Value>, Error>>()?;
+        Ok(Value::Array(<T as ToSpanner>::spanner_type(), values))
+    }
+    fn spanner_type() -> Type {
+        Type::Array(Box::new(<T as ToSpanner>::spanner_type()))
+    }
+}
+
+impl<T> ToSpanner for &[T]
+where
+    T: ToSpanner,
+{
+    fn to_spanner(&self) -> Result<Value, Error> {
+        let values = self
+            .iter()
+            .map(|v| v.to_spanner())
+            .collect::<Result<Vec<Value>, Error>>()?;
+        Ok(Value::Array(<T as ToSpanner>::spanner_type(), values))
+    }
+    fn spanner_type() -> Type {
+        Type::Array(Box::new(<T as ToSpanner>::spanner_type()))
+    }
+}
+
 simple_to!(i8, i64_from);
 simple_to!(u8, i64_from);
 simple_to!(i16, i64_from);
@@ -53,3 +85,55 @@ simple_to!(u32, i64_from);
 simple_to!(i64, i64_from);
 simple_to!(String, String, clone);
 simple_to!(&str, String, v, v.to_string());
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! simple_test_int64 {
+        ($t:ty) => {
+            assert_eq!((0 as $t).to_spanner().ok(), Some(Value::Int64(0)));
+        };
+        ($($t:ty),+) => {
+            $(
+                simple_test_int64!($t);
+            )+
+        };
+    }
+
+    #[test]
+    fn test_to_spanner_simple_int64() {
+        simple_test_int64!(i8, u8, i16, u16, i32, u32, i64);
+    }
+
+    #[test]
+    fn test_to_spanner_opt() {
+        let some = Some(0 as u32);
+        assert_eq!(some.to_spanner().ok(), Some(Value::Int64(0)));
+        let none: Option<u32> = None;
+        assert_eq!(none.to_spanner().ok(), Some(Value::Null(Type::Int64)));
+    }
+
+    #[test]
+    fn test_to_spanner_array() {
+        let array = vec![0, 1, 2, 3, 4];
+        assert_eq!(
+            array.to_spanner().ok(),
+            Some(Value::Array(
+                Type::Int64,
+                vec![
+                    Value::Int64(0),
+                    Value::Int64(1),
+                    Value::Int64(2),
+                    Value::Int64(3),
+                    Value::Int64(4)
+                ]
+            ))
+        );
+        let empty: Vec<u32> = vec![];
+        assert_eq!(
+            empty.to_spanner().ok(),
+            Some(Value::Array(Type::Int64, vec![]))
+        );
+    }
+}
