1f9be5a4cdda79339a5dbdd21b5e65e8112494eb
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-10-04 23:28:37 -0400

    doc: adds crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,93 @@
+//! An asynchronous client for the Cloud Spanner database.
+//!
+//! # Example
+//!
+//! ```no_run
+//! use spanner_rs::{Client, Error};
+//!
+//! #[tokio::main]
+//! fn main() -> Result<(), Error> {
+//!     let mut client = Client::config()
+//!         .with_instance("my-gcp-project", "my-instance")
+//!         .with_database("my-database")
+//!         .connect();
+//!
+//!     client
+//!         .read_write()
+//!         .run(|tx| {
+//!             tx.execute_update(
+//!                 "CREATE TABLE person(id INT64, name STRING(MAX), data BYTES)",
+//!                 &[],
+//!             )
+//!         })
+//!         .run(|tx| {
+//!             tx.execute_update(
+//!                 "INSERT INTO person(id, name, data) VALUES(@id, @name, NULL)",
+//!                 &[("id", &42), ("name", "ferris")],
+//!             )
+//!         })
+//!         .await?;
+//!
+//!     let result_set = client
+//!         .read_only()
+//!         .execute_sql("SELECT * FROM person")
+//!         .await?;
+//!
+//!     for row in result_set.iter() {
+//!         let id: u32 = row.get_by_name("id")?;
+//!         let name: &str = row.get_by_name("name")?;
+//!         let data: Option<&[u8]> = row.get_by_name("data")?;
+//!
+//!         println!("found person: {} {} {:?}", id, name, data);
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! # Transactions
+//!
+//! Cloud Spanner [supports](https://cloud.google.com/spanner/docs/transactions) several transaction "modes":
+//!
+//! * read-only: provides guaranteed consistency between several reads, cannot write;
+//! * read-write: the only way to write into Cloud Spanner they use a combination of locking and retries and are typically more expensive;
+//! * partitioned DML: these are unsupported by this client at the moment.
+//!
+//! ## Read Only
+//!
+//! Reads are done within "single-use" transactions and can be bounded to determine what data is visible to them, see [TimestampBound].
+//! Reading is done using [ReadContext] which can be obtained using [Client::read_only()] or [Client::read_only_with_bound()].
+//!
+//! Example
+//!
+//! ```rust
+//! let result_set = client
+//!     .read_only()
+//!     .execute_sql("SELECT COUNT(*) AS people FROM person", &[])
+//!     .await?;
+//! let people: u32 = result_set.iter().next().unwrap().get_by_name("people")?;
+//! ```
+//!
+//! ## Read Write
+//!
+//! Read / write transactions are done through [TransactionContext] which extends [ReadContext] to allow writes.
+//! When a transaction that conflicts with another tries to commit, Cloud Spanner will reject one of them let the client know it may retry.
+//! This client encapsulates the necessary retry logic such that applications do not need to implement it themselves.
+//!
+//! Example:
+//!
+//! ```rust
+//! client.read_write().run(|tx| {
+//!     // this closure may be invoked more than once
+//!     Box::pin(async move {
+//!         // read
+//!         let rs = tx.execute_sql("...", &[]).await?;
+//!         // write
+//!         tx.execute_update("...", &[]).await?;
+//!     })
+//! })
+//! ```
+
 pub use crate::client::*;
 pub use crate::config::{Config, ConfigBuilder};
 pub(crate) use crate::connection::Connection;
@@ -8,6 +98,7 @@ pub use crate::resource::*;
 pub use crate::result_set::*;
 pub(crate) use crate::session::*;
 pub use crate::to_spanner::*;
+pub use crate::transaction::TimestampBound;
 pub(crate) use crate::transaction::*;
 pub use crate::types::*;
 pub use crate::value::*;
