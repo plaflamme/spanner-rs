29b972c5d98a01735d3eb87ec50a61147a6a442d
plaflamme <plaflamme@users.noreply.github.com>
2022-07-29 00:09:05 +0000

    deploy: 3a2ad90598141fd3c2ec724f993db89e43d0863f

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["spanner_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"spanner_rs/enum.Error.html\" title=\"enum spanner_rs::Error\">Error</a>","synthetic":false,"types":["spanner_rs::error::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var sourcesIndex = {};
+sourcesIndex["spanner_rs"] = {"name":"","dirs":[{"name":"connection","files":["grpc.rs"]}],"files":["auth.rs","client.rs","config.rs","connection.rs","error.rs","from_spanner.rs","lib.rs","resource.rs","result_set.rs","session.rs","statement.rs","to_spanner.rs","transaction.rs","types.rs","value.rs"]};
+createSourceSidebar();
